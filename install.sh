8c49f505fe4f0fc32c5dbb7106844d844af8efd0
Minoru Miyata <miyarappo@gmail.com>
2019-02-06 14:06:41 +0900

    Install node

--- install.sh
+++ install.sh
@@ -2,6 +2,21 @@
 
 echo "start setup..."
 
+# HomeBrewのインストール
+if [ ! -x "`which brew`" ]; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew update
+fi
+
+# nodebrewのインストール
+brew install nodebrew
+$ echo 'export PATH=$HOME/.nodebrew/current/bin:$PATH' >> ~/.bash_profile
+
+# nodeインストール
+nodebrew install-binary v10.10.0
+nodebrew use v10.10.0
+
+# シンボリックを貼る
 for f in .??*; do
     # ignore files
     [ "$f" = ".git" ] && continue
